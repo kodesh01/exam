a1a551e836b6d12d4998ec9c6a527705fd8c8de3
kodesh01 <141357071+kodesh01@users.noreply.github.com>
2024-04-24 22:54:53 +0530

    Delete nlp/nlp directory

--- nlp/nlp/ex2.py
+++
@@ -1,7 +0,0 @@
-from nltk import word_tokenize
-sentence = ['Hi there','How are u']
-for text in sentence:
-    print('Given sentence : ',text)
-    tokens = word_tokenize(text)
-    for token in tokens:
-        print(f'** {token}')
